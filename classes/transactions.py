1bbd8dfaf87d0cf2eac81ff6ee526483f09db478
s-bye <kill46923@gmail.com>
2025-03-31 15:12:09 +0600

    Splited transactions and transactionsDAO

--- classes/transactions.py
+++ classes/transactions.py
@@ -1,6 +1,3 @@
-import sqlite3
-from datetime import datetime
-
 class Transaction:
     def __init__(self, transaction_id, user_id, transaction_type, amount, timestamp):
         self.transaction_id = transaction_id
@@ -12,42 +9,3 @@ class Transaction:
     def __repr__(self):
         return f"Transaction(transaction_id={self.transaction_id}, user_id={self.user_id}, " \
                f"transaction_type={self.transaction_type}, amount={self.amount}, timestamp={self.timestamp})"
-
-class TransactionDAO:
-    def __init__(self, db_file="bank_database.sqlite"):
-        self.db_file = db_file
-
-    def connect_db(self):
-        return sqlite3.connect(self.db_file)
-
-    def add_transaction(self, user_id, transaction_type, amount):
-        conn = self.connect_db()
-        cursor = conn.cursor()
-
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cursor.execute('''INSERT INTO transactions (user_id, transaction_type, amount, timestamp)
-                      VALUES (?, ?, ?, ?)''', (user_id, transaction_type, amount, timestamp))
-
-        conn.commit()
-        conn.close()
-
-    def get_transactions_by_user_id(self, user_id):
-        conn = self.connect_db()
-        cursor = conn.cursor()
-
-        cursor.execute("""SELECT * FROM transactions WHERE user_id = ? ORDER BY timestamp DESC""", (user_id,))
-        rows = cursor.fetchall()
-        conn.close()
-
-        transactions = [Transaction(*row) for row in rows]
-        return transactions
-
-    def get_all_transactions(self):
-        conn = self.connect_db()
-        cursor = conn.cursor()
-
-        cursor.execute("""SELECT * FROM transactions ORDER BY timestamp DESC""")
-        transactions = cursor.fetchall()
-        conn.close()
-        return transactions
-
---
+++ classes/transactionsDAO.py
@@ -0,0 +1,42 @@
+import sqlite3
+from datetime import datetime
+from classes.transactions import Transaction
+
+class TransactionDAO:
+    def __init__(self, db_file="bank_database.sqlite"):
+        self.db_file = db_file
+
+    def connect_db(self):
+        return sqlite3.connect(self.db_file)
+
+    def add_transaction(self, user_id, transaction_type, amount):
+        conn = self.connect_db()
+        cursor = conn.cursor()
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('''INSERT INTO transactions (user_id, transaction_type, amount, timestamp)
+                      VALUES (?, ?, ?, ?)''', (user_id, transaction_type, amount, timestamp))
+
+        conn.commit()
+        conn.close()
+
+    def get_transactions_by_user_id(self, user_id):
+        conn = self.connect_db()
+        cursor = conn.cursor()
+
+        cursor.execute("""SELECT * FROM transactions WHERE user_id = ? ORDER BY timestamp DESC""", (user_id,))
+        rows = cursor.fetchall()
+        conn.close()
+
+        transactions = [Transaction(*row) for row in rows]
+        return transactions
+
+    def get_all_transactions(self):
+        conn = self.connect_db()
+        cursor = conn.cursor()
+
+        cursor.execute("""SELECT * FROM transactions ORDER BY timestamp DESC""")
+        transactions = cursor.fetchall()
+        conn.close()
+        return transactions
+
