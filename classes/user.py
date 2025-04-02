100ebd0f9a6480a73f44a2d9c761302168995856
Baiastan Zamirbekov <baastan569@gmail.com>
2025-04-02 14:38:14 +0600

    Updated some parts

--- classes/user.py
+++ classes/user.py
@@ -6,7 +6,11 @@ class User:
         self.__pin_code = pin_code
         self.__balance = balance
 
-    def get_pin_code(self):
+
+    def __str__(self):
+        return f"User(user_id={self.user_id}, full_name={self.full_name}, card_number={self.card_number}, pin_code={self.__pin_code}, balance={self.__balance})"
+
+    def get_pincode(self):
         return self.__pin_code
 
     def set_pin_code(self, new_pin_code):
@@ -18,8 +22,16 @@ class User:
     def set_balance(self, new_balance):
         self.__balance = new_balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount < self.__balance:
+            self.__balance -= amount
+            return True
+        return False
     def __repr__(self):
         return f"User(user_id={self.user_id}, full_name='{self.full_name}', card_number='{self.card_number}', balance={self.__balance})"
-
-
-
