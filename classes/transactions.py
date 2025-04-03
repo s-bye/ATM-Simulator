ede77a027783532601bffb625fe53ce5c21df97c
Baiastan Zamirbekov <baastan569@gmail.com>
2025-04-03 12:08:59 +0600

    Updated to private type

--- classes/transactions.py
+++ classes/transactions.py
@@ -3,9 +3,9 @@ class Transaction:
         self.transaction_id = transaction_id
         self.user_id = user_id
         self.transaction_type = transaction_type
-        self._amount = amount
+        self.__amount = amount
         self.timestamp = timestamp
 
     def __repr__(self):
         return f"Transaction(transaction_id={self.transaction_id}, user_id={self.user_id}, " \
-               f"transaction_type={self.transaction_type}, amount={self.amount}, timestamp={self.timestamp})"
+               f"transaction_type={self.transaction_type}, amount={self.__amount}, timestamp={self.timestamp})"
